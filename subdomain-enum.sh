abdf1b71dd19d1f6d850197f18fe965b4094234f
Devang Solanki <75718583+Devang-Solanki@users.noreply.github.com>
2022-04-16 22:52:18 +0530

    Added support for arguments

--- subdomain-enum.sh
+++ subdomain-enum.sh
@@ -7,9 +7,64 @@ Green='\033[0;32m'        # Green
 Yellow='\033[0;33m'       # Yellow
 Blue='\033[0;34m'         # Blue
 
+# Help                                                     #
+############################################################
 
+Help()
+{
+   # Display Help
+   echo -e "${Yellow}[*] Subdomain enum script for finding subdomain and sorting on basis of status code.${NC}"
+   echo
+   echo -e "${Green}Syntax: subdomain-enum.sh${NC} ${Blue}[-l|h|]${NC}"
+   echo -e "${Green}options:${NC}"
+   echo -e "${Red}l :${NC}     ${Blue}Takes list of domain as input.${NC}"
+   echo -e "${Red}h :${NC}     ${Blue}Print this Help.${NC}"
+   echo
+   echo -e "${Green}example subdomain-enum.sh -l example.org.txt${NC}"
+}
 
-#Checking if all tools are installed properly
+# Getting the list of domains                              #
+############################################################
+
+Domain_list()
+{
+    List=$1
+
+    if [[ -f $List && -s $List ]]
+    then
+         echo -e "Running script for file : $List"
+    elif [[ -f $List && ! -s $List ]]
+    then
+         echo -e "${Red}Error :${NC} File $List seems to be empty"
+         exit
+    else
+         echo -e "${Red}Error :${NC} File $List does not exist"
+	 exit
+    fi
+
+}
+
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+while getopts "hl:" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      l) # Getting the list of domains
+      	 File=$OPTARG;;
+      \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+Domain_list "$File"
+
+# Checking if all tools are installed properly             #
+############################################################
+
 echo -e "${Yellow}[*] Checking if all tools are installed properly\n${NC}"
 
 if type amass &> /dev/null
@@ -74,8 +129,10 @@ fi
 
 
 
-#Checking if its running for second time
-#Building the env
+# Checking if its running for second time                  #
+# Building the env                                         #
+############################################################
+
 echo -e "${Yellow}[*] Checking if its runned before\n"
 
 if [[ ! -d "$PWD/old" ]]
@@ -110,53 +167,57 @@ then
 fi
 
 
-#Finding all subdomain and storing it in all_subdomain.txt
+# Finding all subdomain and storing it in all_subdomain.txt #
+#############################################################
+
 echo -e "${Yellow}[*] Finding all subdomain and storing it in all_subdomain.txt\n${NC}"
 
-#Subdomains from subfinder
+# Subdomains from subfinder
 echo -e "${Blue}Running Subfinder\n${NC}"
 
-subfinder -all -dL $1 -o "/tmp/subf_$1"  -silent
+subfinder -all -dL $List -o "/tmp/subf_$List"  -silent
 
-#Subdomains from amass
+# Subdomains from amass
 echo -e "\n${Blue}Running Amass"
 
-amass enum -passive -df $1 -src -config ~/.config/amass/config.ini -silent -o "/tmp/amass_$1" 
+amass enum -passive -df $List -src -config ~/.config/amass/config.ini -silent -o "/tmp/amass_$List" 
 
-#Subdomains from findomain
+# Subdomains from findomain
 echo -e "\n${Blue}Running Findomain${NC}"
 
-findomain -f $1 -u "/tmp/find_$1"
+findomain -f $List -u "/tmp/find_$List"
 
-#Subdomains from gauplus
+# Subdomains from gauplus
 echo -e "${Blue}Running Gauplus for subdomain\n${NC}"
 
-cat $1 | gauplus -subs | unfurl -u domains | tee -a "/tmp/gau_$1"
+cat $List | gauplus -subs | unfurl -u domains | tee -a "/tmp/gau_$List"
 
 echo -e "\n${Yellow}[*] Prcosessing all files generated from tools\n${NC}"
 
-#Fomating files from amass for combining
-cat "/tmp/amass_$1" | cut -f 2 -d ']' | sed 's/ //g' | tee -a "/tmp/amass_final_$1"
+# Fomating files from amass for combining
+cat "/tmp/amass_$List" | cut -f 2 -d ']' | sed 's/ //g' | tee -a "/tmp/amass_final_$List"
 
-#Combining all the files
+# Combining all the files
 echo -e "\n${Blue}Combining all the files\n${NC}"
 
-cat "/tmp/subf_$1" "/tmp/find_$1" "/tmp/amass_final_$1" "/tmp/gau_$1" | sort -u | tee -a "$PWD/all_subdomain.txt"
+cat "/tmp/subf_$List" "/tmp/find_$List" "/tmp/amass_final_$List" "/tmp/gau_$List" | sort -u | tee -a "$PWD/all_subdomain.txt"
 
-#Running httpx for all status code
+# Running httpx for all status code                         #
+############################################################
+
 echo -e "\n${Yellow}[*] Running httpx and sorting domains based on status code\n${NC}"
 
 httpx -l "$PWD/all_subdomain.txt" -title -status-code -tech-detect -no-color -o "$PWD/httpx_url.txt"
 
-#Sorting 200,301,302 etc live domains
+# Sorting 200,301,302 etc live domains
 echo -e "\n${Blue}Sorting all 20x,30x etc live domains\n${NC}"
 
 sed -e "/[23][0-9][0-9]/{w $PWD/live.txt" -e 'd}' "$PWD/httpx_url.txt"
 
-#Sorting 404 for possible subdomain takeover
+# Sorting 404 for possible subdomain takeover
 echo -e "\n${Blue}Sorting 404 for possible subdomain takeover\n${NC}"
 sed -e "/404/{w $PWD/404.txt" -e 'd}' "$PWD/httpx_url.txt"
 
-#Sorting 403 domains
+# Sorting 403 domains
 echo -e "\n${Blue}Sorting 403 domains\n${NC}"
 sed -e "/403/{w $PWD/403.txt" -e 'd}' "$PWD/httpx_url.txt"
